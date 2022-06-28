4debf5f92780aa06bf5d62df8afcf23a39d65949
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2022-06-28 11:51:53 +0700

    Set blacklisted IPs via environment variable

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import asdict
 from itertools import groupby
 from operator import attrgetter
@@ -26,8 +27,6 @@ class Settings(BaseSettings):
     cdn_cache_interval: int = 30
 
 
-BLACKLISTED_CLIENTS = ('34.68.53.144',)
-
 logger = Logger(__name__)
 app = FastAPI(title='Vietnam Provinces online API', version=__version__)
 api = APIRouter()
@@ -49,9 +48,12 @@ async def show_all_divisions(request: Request,
                                                 title='Show down to subdivisions',
                                                 description='2: show districts; 3: show wards')):
     client_ip = request.client.host
-    if depth > 1 and client_ip in BLACKLISTED_CLIENTS:
-        logger.info('{} is blacklisted.', client_ip)
-        raise HTTPException(429)
+    if depth > 1:
+        env_value = os.getenv('BLACKLISTED_CLIENTS', '')
+        blacklist = (s.strip() for s in env_value.split(','))
+        if client_ip in blacklist:
+            logger.info('{} is blacklisted.', client_ip)
+            raise HTTPException(429)
     if depth >= 3:
         return FileResponse(NESTED_DIVISIONS_JSON_PATH)
     if depth == 2:
